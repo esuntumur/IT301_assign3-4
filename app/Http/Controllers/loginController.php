ca4ee7e2858d405c7799c51f69e3bbfe079b78f5
esuntumur <esontomorgantomor@gmail.com>
2021-04-25 16:21:34 +0800

    login change master files

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -13,15 +13,6 @@ class loginController extends Controller
     {
         return view('login.login');
     }
-    public function loginSearchByType($type, $request)
-    {
-        $exist = DB::table($type)->get();
-        foreach ($exist as $item) {
-            if ($item->name == $request->name && $item->password == $request->password) {
-                return view('homes.adminHome', ['name' => $request->name, 'password' => $request->password]);
-            } else return redirect()->back()->withSuccess("Таны оруулсан хэрэглэгч байхгүй байна.");
-        }
-    }
     public function doLogin(Request $request)
     {
         // TODO 1 => DB select checking
@@ -32,7 +23,13 @@ class loginController extends Controller
         $exist = DB::table($request->loginType)->get();
         foreach ($exist as $item) {
             if ($item->name == $request->name && $item->password == $request->password) {
-                return view('homes.adminHome', ['name' => $request->name, 'password' => $request->password]);
+                if ($request->loginType == 'admin') {
+                    return view('layout.masterAdmin', ['name' => $request->name, 'password' => $request->password]);
+                } else if ($request->loginType == 'customer') {
+                    return view('layout.masterCustomer', ['name' => $request->name, 'password' => $request->password]);
+                } else if ($request->loginType == 'shop') {
+                    return view('layout.masterShop', ['name' => $request->name, 'password' => $request->password]);
+                }
             } else return redirect()->back()->withSuccess("Таны оруулсан хэрэглэгч байхгүй байна.");
         }
     }
