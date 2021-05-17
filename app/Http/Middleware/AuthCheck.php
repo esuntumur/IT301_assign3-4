8460f12c939f907efcff1d192884044f6de0a53d
Amaraa Baigal <blackjok28@gmail.com>
2021-05-17 09:44:44 +0800

    .

--- app/Http/Middleware/AuthCheck.php
+++ app/Http/Middleware/AuthCheck.php
@@ -17,10 +17,12 @@ class AuthCheck
      */
     public function handle(Request $request, Closure $next)
     {
+
         if ((!session()->has('LoggedCustomer') && !session()->has('LoggedAdmin') && !session()->has('LoggedShop')) && ($request->path() != 'auth/login' && $request->path() != 'auth/register' && $request->path() != '/')) {
             return redirect('auth/login')->with('fail', 'нэвтрэх шаадлагатай');
         }
-        if (session()->has('LoggedCustomer') && ($request->path() != 'customer/myorder' && $request->path() != 'customer/home' && $request->path() != 'customer/profile' && $request->path() != 'customer/search' && $request->path() != 'customer/content/{id}' && $request->path() != 'customer/content/{id}/orderContent' && $request->path() != 'customer/myorder/{orderId}')) {
+
+        if (session()->has('LoggedCustomer') && ($request->path() != 'customer/myorder' && $request->path() != 'customer/home' && $request->path() != 'customer/profile' && $request->path() != 'customer/search' && !$request->is('customer/content/*') && $request->path() != 'customer/content/{id}/orderContent' && $request->path() != 'customer/myorder/{orderId}')) {
             return back();
         }
         if (session()->has('LoggedAdmin') && ($request->path() != 'admin/home' && $request->path() != 'admin/profile' && $request->path() != 'admin/shops' && $request->path() != 'admin/customers')) {
